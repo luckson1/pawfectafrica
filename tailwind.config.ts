0c13a481c3d7e24c5dc84c618f35056eaf18c425
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-08 12:10:03 +0300

    feat: add daisyUI for faster styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,22 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+ 
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#46518C",
+         " primary-content": "#FFFFFF",
+          secondary: "#D9A648",
+          accent: "#F2D49B",
+          neutral: "#3d4451",
+          "base-100": "#fff",
+        },
+      },
+      "dark",
+   
+    ],
+  },
 } satisfies Config;
