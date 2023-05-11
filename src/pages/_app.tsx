64bf8fd381c097d7652a9b85f4e78f73e6583e56
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-11 08:39:27 +0300

    feat: add layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,18 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Layout from "../components/layout";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
+
     <SessionProvider session={session}>
+      <Layout>
       <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 };
