0dc74136099cd00689698e5d9d8b8a25aca6e5a4
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-19 18:19:54 +0300

    fix: dark mode colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type Config } from "tailwindcss";
 
 export default {
@@ -12,14 +15,24 @@ export default {
       {
         mytheme: {
           primary: "#46518C",
+          "primary-focus": '#333f7a',
          " primary-content": "#FFFFFF",
+         "secondary-focus": '#a37115',
           secondary: "#D9A648",
           accent: "#F2D49B",
           neutral: "#3d4451",
           "base-100": "#fff",
         },
+        dark: {
+          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
+          primary: "#46518C",
+          "primary-focus": '#333f7a',
+          secondary: "#D9A648",
+          "secondary-focus": '#a37115',
+          accent: "#F2D49B",
+        }
       },
-      "dark",
+    
    
     ],
   },
